c62133bc447e805ae328fed751b8476a36efbe0f
Abubakr7 <abubakr.juraev98@gmail.com>
2024-03-22 20:41:33 +0500

    add response video type

--- src/types/requests/uploadVideo.d.ts
+++ src/types/requests/uploadVideo.d.ts
@@ -4,11 +4,22 @@
  * @author: exode <hello@exode.ru>
  */
 
+interface VideoKinescopeResponse {
+    id: string,
+    parent_id: string,
+    title: string,
+    description: string,
+    status: string,
+    play_link: string,
+    embed_link: string,
+    hls_link: string,
+    created_at: string
+}
 export interface UploadVideoByFileResponse {
-
+    data: VideoKinescopeResponse
 }
 
 
 export interface UploadVideoByUrlResponse {
-
+    data: VideoKinescopeResponse
 }
